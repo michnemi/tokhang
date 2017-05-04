ad96366344bead0152f9cec224bff8bcc83569c7
michirou <michnem04@gmail.com>
2017-05-04 15:57:10 +0800

    new chnages

--- js/script.js
+++ js/script.js
@@ -1,14 +1,54 @@
 var time = 0;
 var timed;
 var pause_time = false;
+
 window.onload = function(){
+	setTimeout(startLanes, 1000);
+	setTimeout(generateCars, 1000);
+
 	document.addEventListener('keypress', carMove);	
 	document.getElementById("home").addEventListener('click', back2home);
 	document.getElementById("pause").addEventListener('click', paused);
 	setTimeout(timeStarter,3000);
-	
 }
 
+function generateCars(){
+	var timeInterval = getInterval();
+
+	setTimeout(spawnCar, timeInterval);
+
+console.log(timeInterval);
+}
+
+function spawnCar(){
+	console.log("here na po");
+	var car = document.getElementById("cars");
+	var car_num = generateCarNum();
+
+	car.src = "../images/car"+ car_num + ".png";
+	car.style.animation = "car_animation 5s linear";
+}
+
+
+function generateCarNum(){
+	var car_num = Math.floor((Math.random() * 9))+ 1;
+	return car_num;
+}
+
+function getInterval(){
+	var interval = (Math.floor((Math.random() * 10) + 1)) * 1000;
+	return interval; 
+}
+
+function startLanes(){
+	var lane1 = document.getElementById("lane1");
+	var lane2 = document.getElementById("lane2");
+	var lane3 = document.getElementById("lane3");
+
+	lane1.style.animation = "lane_animation1 1s linear infinite";
+	lane2.style.animation = "lane_animation2 1s linear infinite";
+	lane3.style.animation = "lane_animation3 1s linear infinite";
+}
 
 
 
